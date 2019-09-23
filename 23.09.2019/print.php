82bc0675f2518fcca90a3652cda9b60c61394479
Sven Haavik <sven.haavik@khk.ee>
2019-09-23 11:39:45 +0300

    teksti väljastus print ja echo abil

---
+++ 23.09.2019/print.php
@@ -0,0 +1,8 @@
+<?php
+// printimine print funktsiooni abil
+print "See on esimene php rida<br>";
+print "See on teine php rida<br>";
+print "See on kolmas php rida<br>";
+
+// echo abil printimine
+echo 'See on tekst, mis on kirjutatud echo abil';
